b70527c77e7bfd2443d33df4efe37af0a60be1d1
Maksym Zinchenko <maklaut98@gmail.com>
2022-01-20 17:36:41 -0600

    menu for mobile devices

---
+++ src/components/HamburgerIcon/HamburgerIcon.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Hamburger = styled.div`
+  display: none;
+  @media (max-width: 767px){
+    width: 2rem;
+    height: 2rem;
+    display: flex;
+    justify-content: space-around;
+    flex-flow: column nowrap;
+    z-index: 10;
+  }
+  
+`;
+const Burger = styled.div`
+  width: 2rem;
+  height: 0.25rem;
+  border-radius: 10px;
+  background-color: white;
+  transform-origin: 1px;
+  transition: all 0.3s linear;
+`;
+
+function HamburgerIcon() {
+  return (
+    <Hamburger>
+      <Burger />
+      <Burger />
+      <Burger />
+    </Hamburger>
+  );
+}
+
+export default HamburgerIcon;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import HamburgerIcon from '../HamburgerIcon/HamburgerIcon';
+
+interface menuButton {
+  isOpen: boolean;
+}
 
 const HeaderWrap = styled.header`
   display: flex;
-  width: 100%;
-  height: 70px;
+  height: 60px;
   font-size:  1.2em;
   background-color: #3687FF;
   color: #fff;
   font-weight: 600;
   align-items: center;
+  position: relative;
+  justify-content: space-between;
 `;
-const Navbar = styled.nav`
-  display: flex;
+const Navbar = styled.nav<menuButton>`
+  display: ${(props) => (props.isOpen ? 'flex' : 'none')};
   width: 70%;
   justify-content: space-around;
+  @media (min-width: 767px){
+    display: flex;
+  }
+  @media (max-width: 767px){
+    height: ${(props) => (props.isOpen ? 'flex' : 'none')};
+    position: absolute;
+    flex-direction: column;
+    align-items: flex-end;
+    top: 100%;
+    width: calc(100% - 20px);
+    background-color: #3687FF;
+    padding: 5px 10px;
+  }
 `;
 const Logo = styled.div`
   display: flex;
@@ -25,15 +44,30 @@ const Logo = styled.div`
   font-family: "Mistral";
   font-size: 1.5em;
 `;
+const MenuButton = styled.div`
+  margin-right: 10px;
+`;
+const StyledLink = styled(Link)`
+  color: white;
+  padding: 8px;
+`;
+
 function Header() {
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
     <HeaderWrap className="Header">
       <Logo>Zinmax</Logo>
-      <Navbar className="navbar">
-        <Link to="/">Home</Link>
-        <Link to="/portfolio">Portfolio</Link>
-        <Link to="/contact">Contact</Link>
+      <Navbar isOpen={menuOpen} className="navbar">
+        <StyledLink to="/">Home</StyledLink>
+        <StyledLink to="/portfolio">Portfolio</StyledLink>
+        <StyledLink to="/contact">Contact</StyledLink>
       </Navbar>
+      <MenuButton onClick={toggleMenu}>
+        <HamburgerIcon />
+      </MenuButton>
     </HeaderWrap>
   );
 }
